e228b9846ffd1d5e95bf71b7de38c11d499c276b
danburg-dev <danburg.nl@gmail.com>
2021-07-03 12:34:37 +0200

    opdracht 1 functions.js afgerond

--- functions.js
+++ functions.js
@@ -14,20 +14,50 @@ console.log(greeting);
 // Schrijf een functie genaamd getSchool, die de string "Novi Hogeschool" teruggeeft
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Novi Hogeschool"
 
+function getSchool() {
+  return "Novi Hogeschool";
+}
+
+const hi = getSchool();
+
+console.log(hi);
 
 
 /* Opdracht 2 */
 // Schrijf een functie genaamd compliment, die jou een complimentje geeft!
 // ---- Verwachte uitkomst bij het aanroepen van de functie: "Lekker bezig met die functies, Jan!"
 
+function compliment() {
+  return 'Lekker bezig met die functies, Daniel!';
+}
 
+const shoulderPet = compliment();
+
+console.log(shoulderPet);
 
 /* Opdracht 3 */
 // Schrijf een functie genaamd getGrades, die een array met de cijfers 8, 8.5, 6 en 7 teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: [8, 8.5, 6, 7]
 
+function getGrades() {
+  return [8,8.5, 6, 7];
+}
 
+const showGrades = getGrades();
+
+console.log(showGrades);
 
 /* Opdracht 4 */
 // Schrijf een functie genaamd getDetails, die een object met daarin jouw eigen naam en achternaam teruggeeft.
 // ---- Verwachte uitkomst bij het aanroepen van de functie: { firstName: 'Jan', lastName: 'Janssen' }
+
+function getDetails() {
+  return nawData = {
+    firstName: 'Daniel',
+    lastName: 'Burgemeestre'
+  };
+}
+const newData = getDetails();
+
+console.log(newData);
+console.log(newData.firstName + ' ' + newData.lastName);
